3876ea0c0c5ecf7532e37af67cfa4cf99e01487d
Dillon Ellis <dillonzellis@gmail.com>
2022-10-04 12:02:09 -0400

    add scroll effect on desktop nav

--- js/tseting.js
+++
@@ -1,7 +0,0 @@
-const primaryNav = document.querySelector(".primary-navigation");
-const navToggle = document.querySelector(".mobile-nav-toggle");
-
-
-navToggle.addEventListener('click', ()=>{
-    
-})
